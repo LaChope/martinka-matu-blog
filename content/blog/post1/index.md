f43a51496ffd512e5d44533ad8cd9cb35c323791
Max Chopart <max.chopart@gmail.com>
2022-01-12 14:21:56 +0100

    Add index blog page and change blog-post layout

---
+++ content/blog/post1/index.md
@@ -0,0 +1,13 @@
+---
+title: "My First Post"
+date: "2021-07-23"
+image: "./post1.jpg"
+hero_image_alt: "tropics sea"
+hero_image_credit_text: "Me"
+hero_title: "My first post"
+---
+This is my first blog post! Isn't it *great*?
+Some of my **favorite** things are:
+* Petting dogs
+* Singing
+* Eating potato-based foods
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,68 +2,77 @@ import * as React from "react"
 import {graphql, Link} from "gatsby"
 
 import Bio from "../components/Bio"
-import LayoutOld from "../components/layoutOld"
 import SEO from "../components/SEO"
 import {GatsbyImage, getImage} from "gatsby-plugin-image";
+import Layout from "../components/Layout";
+import HeroVideo from "../components/HeroVideo";
+import HeroImage from "../components/HeroImage";
 
-const BlogPostTemplate = ({data, location}) => {
+const BlogPostTemplate = ({data}) => {
     const post = data.markdownRemark
     const siteTitle = data.site.siteMetadata?.title || `Title`
     const {previous, next} = data
     const image = getImage(data.markdownRemark.frontmatter.image)
 
     return (
-        <LayoutOld location={location} title={siteTitle}>
+        <Layout pageTitle={siteTitle}>
             <SEO
                 title={post.frontmatter.title}
                 description={post.frontmatter.description || post.excerpt}
             />
-            <article
-                className="blog-post"
-                itemScope
-                itemType="http://schema.org/Article"
-            >
-                <header>
-                    <h1 itemProp="headline">{post.frontmatter.title}</h1>
-                    <p>{post.frontmatter.date}</p>
-                </header>
-                <section
-                    dangerouslySetInnerHTML={{__html: post.html}}
-                    itemProp="articleBody"
-                />
-                <GatsbyImage alt="test" image={image}/>
-                <hr/>
-                <footer>
-                    <Bio/>
-                </footer>
-            </article>
-            <nav className="blog-post-nav">
-                <ul
-                    style={{
-                        display: `flex`,
-                        flexWrap: `wrap`,
-                        justifyContent: `space-between`,
-                        listStyle: `none`,
-                        padding: 0,
-                    }}
-                >
-                    <li>
-                        {previous && (
-                            <Link to={previous.fields.slug} rel="prev">
-                                ← {previous.frontmatter.title}
-                            </Link>
-                        )}
-                    </li>
-                    <li>
-                        {next && (
-                            <Link to={next.fields.slug} rel="next">
-                                {next.frontmatter.title} →
-                            </Link>
-                        )}
-                    </li>
-                </ul>
-            </nav>
-        </LayoutOld>
+            {post.frontmatter.video && <HeroVideo videoSrcUrl={post.frontmatter.video} videoTitle={post.frontmatter.video_title} heroTitle={post.frontmatter.hero_title}/>}
+            {image && <HeroImage gatsbyImage={image} imageAlt={post.frontmatter.hero_image_alt} heroTitle={post.frontmatter.hero_title}/>}
+            <p>{post.frontmatter.date}</p>
+            <section
+                dangerouslySetInnerHTML={{__html: post.html}}
+                itemProp="articleBody"
+            />
+            {/*<article*/}
+            {/*    className="blog-post"*/}
+            {/*    itemScope*/}
+            {/*    itemType="http://schema.org/Article"*/}
+            {/*>*/}
+            {/*    <header>*/}
+            {/*        <h1 itemProp="headline">{post.frontmatter.title}</h1>*/}
+            {/*        <p>{post.frontmatter.date}</p>*/}
+            {/*    </header>*/}
+            {/*    <section*/}
+            {/*        dangerouslySetInnerHTML={{__html: post.html}}*/}
+            {/*        itemProp="articleBody"*/}
+            {/*    />*/}
+            {/*    <GatsbyImage alt="test" image={image}/>*/}
+            {/*    <hr/>*/}
+            {/*    <footer>*/}
+            {/*        <Bio/>*/}
+            {/*    </footer>*/}
+            {/*</article>*/}
+            {/*<nav className="blog-post-nav">*/}
+            {/*    <ul*/}
+            {/*        style={{*/}
+            {/*            display: `flex`,*/}
+            {/*            flexWrap: `wrap`,*/}
+            {/*            justifyContent: `space-between`,*/}
+            {/*            listStyle: `none`,*/}
+            {/*            padding: 0,*/}
+            {/*        }}*/}
+            {/*    >*/}
+            {/*        <li>*/}
+            {/*            {previous && (*/}
+            {/*                <Link to={previous.fields.slug} rel="prev">*/}
+            {/*                    ← {previous.frontmatter.title}*/}
+            {/*                </Link>*/}
+            {/*            )}*/}
+            {/*        </li>*/}
+            {/*        <li>*/}
+            {/*            {next && (*/}
+            {/*                <Link to={next.fields.slug} rel="next">*/}
+            {/*                    {next.frontmatter.title} →*/}
+            {/*                </Link>*/}
+            {/*            )}*/}
+            {/*        </li>*/}
+            {/*    </ul>*/}
+            {/*</nav>*/}
+        </Layout>
     )
 }
 
@@ -88,6 +97,9 @@ export const pageQuery = graphql`
         title
         date(formatString: "MMMM DD, YYYY")
         description
+        hero_image_alt
+        hero_title
+        video
         image {
             childImageSharp {
               gatsbyImageData(layout: CONSTRAINED)
